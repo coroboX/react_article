9d5621c916f0b05395e78c6b1cc9b0e8dc38b2f4
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-23 18:00:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.91664e296ef69cc2f7485705d49e2a04.js"
 );
 
 self.addEventListener('message', (event) => {
